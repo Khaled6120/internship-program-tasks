90c678695436e6373106cd70b7cfa4217b4d481e
Khaled6120 <khalednaes3@gmail.com>
2023-10-19 00:21:11 +0300

    finish task 7

---
+++ Dog-years-task-7/script.js
@@ -0,0 +1,11 @@
+let myAge = 23; // My age.
+let earlyYears = 2; // The value saved to this variable will change.
+earlyYears *= 10.5; // The first two years of a dog’s life count as 10.5 dog years each.
+let laterYears = myAge - 2; // Subtract 2 from myAge variable.
+laterYears *= 4; // Each year following equates to 4 dog years.
+
+console.log(earlyYears, laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears; // My age in dog years.
+let myName = 'Khaled'.toLowerCase(); // My name in lowercase letters.
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`); // My name and my age in dog years.
